b1f11542aed81cd933efc22eefea104fab32502a
robi000 <smwefyelem@gmail.com>
2023-03-16 09:32:47 +0300

    on progress

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
--- creates a user and grants specific permission to them
+-- create user two with read previlage
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 REVOKE ALL PRIVILEGES ON *.* FROM 'user_0d_2'@'localhost';
