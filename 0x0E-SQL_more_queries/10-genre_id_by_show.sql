b53d91229827c575522e36b8acff482a706daca4
robi000 <smwefyelem@gmail.com>
2023-03-16 10:59:51 +0300

    on progress

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,2 @@
+-- use inner join
+select tv_shows.title , tv_show_genres.genre_id from tv_shows inner join tv_show_genres on tv_shows.id = tv_show_genres.show_id order by tv_shows.title , tv_show_genres.genre_id;
