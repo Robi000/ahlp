9cd675a66798706b632af944f5d31d00b7703171
robi000 <smwefyelem@gmail.com>
2023-03-16 10:13:09 +0300

    on progress

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- display all city  with their state
-SELECT cities.id AS id , cities.name AS name , states.name As name from  cities , states WHERE cities.state_id = states.id ORDER BY cities.id;
+SELECT cities.id AS id , cities.name AS name , states.name AS name FROM  cities , states WHERE cities.state_id = states.id ORDER BY cities.id;
